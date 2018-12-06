c2ea5d21ec5fddf0a3d99a4372ceec41fb7d76be
Jonas Wikström <jonaswikstrom@student-215-98.eduroam.uu.se>
2018-12-06 17:26:59 +0100

    jag vill ha axels kod

--- serve.js
+++ serve.js
@@ -37,20 +37,11 @@ data.initializeData();
 
 io.on('connection', function (socket) {
   // Send list of orders and text labels when a client connects
-<<<<<<< HEAD
   socket.on('pageLoaded', function() {
     socket.emit('initialize', { orders: data.getAllOrders(),
                             uiLabels: data.getUILabels(),
                             ingredients: data.getIngredients() });
   });
-
-=======
-socket.on('pageLoaded', function() {
-  socket.emit('initialize', { orders: data.getAllOrders(),
-                          uiLabels: data.getUILabels(),
-                          ingredients: data.getIngredients() });
-});
->>>>>>> c3f2859f32dfee06519669d1a17023e62da8ae36
   // When someone orders something
   socket.on('order', function (order) {
     var orderIdAndName = data.addOrder(order);
