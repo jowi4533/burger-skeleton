1851cb1b7d24ba4dca6829a0840ea7572745ddd7
Jonteton <jonteton@gmail.com>
2018-12-10 16:30:53 +0100

    fixat meny

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
-//import Kitchen from './views/Kitchen.vue'
 import Vue from 'vue'
 import Router from 'vue-router'
 import Ordering from './views/Ordering.vue'
+import Kitchen from './views/Kitchen.vue'
 
 Vue.use(Router)
 
@@ -16,25 +16,14 @@ let router = new Router({
       name: 'ordering',
       component: Ordering
     },
-    // {
-    //   path: '/frontpage',
-    //   meta: {
-    //     title: 'RAW Sauce Burgers',
-    //   },
-    //   name: 'frontpage',
-    //   component: () => import( './views/FrontPage.vue')
-    //
-    // },
+
     {
       path: '/kitchen',
       meta: {
         title: 'Kitchen',
       },
       name: 'kitchen',
-      // route level code-splitting
-      // this generates a separate chunk (read.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "read" */ './views/Kitchen.vue')
+      component: Kitchen
     },
 
   ]
--- src/views/Ordering.vue
+++ src/views/Ordering.vue
@@ -1,24 +1,23 @@
 <template>
-  <div id="ordering">
+  <div>
+    <div v-if = "showMainMenu" >
+      <MenuPage>
+
+      </MenuPage>
+      <button v-on:click= "klappatochKlart()">Klart</button>
+    </div>
+
+
+
+  <div id="ordering" v-if = "showOrdering">
     <img class="example-panel" src="@/assets/exampleImage.jpg">
     <button v-on:click="switchLang()">{{ uiLabels.language }}</button>
 
 
-   <!-- <div id="frontpage">
-      <h1>TAJaa</h1>
-      <FrontPage> hello </FrontPage>
-      <button>Eat here</button>
-      <button>Take away</button>
-
-       v-on:click="placeToEat('Eat here')"
-      v-on:click="placeToEat('Take away')
-
-    </div> -->
-
     <div id="ingredients_">
-    <h1>{{ uiLabels.ingredients }}</h1>
+      <h1>{{ uiLabels.ingredients }}</h1>
 
-    <Ingredient
+      <Ingredient
       ref="ingredient"
       v-for="item in ingredients"
       v-on:increment="addToOrder(item)"
@@ -32,23 +31,24 @@
     {{ chosenIngredients.map(item => item["ingredient_"+lang]).join(', ') }}, {{ price }} kr
     <button v-on:click="placeOrder()">{{ uiLabels.placeOrder }}</button>
   </div>
-<div id="order_item">
+  <div id="order_item">
     <h1>{{ uiLabels.ordersInQueue }} tja</h1>
 
     <h1>{{ uiLabels.ordersInQueue }}</h1>
     <div>
       <OrderItem
-        v-for="(order, key) in orders"
-        v-if="order.status !== 'done'"
-        :order-id="key"
-        :order="order"
-        :ui-labels="uiLabels"
-        :lang="lang"
-        :key="key">
-        hej
-      </OrderItem>
-    </div>
+      v-for="(order, key) in orders"
+      v-if="order.status !== 'done'"
+      :order-id="key"
+      :order="order"
+      :ui-labels="uiLabels"
+      :lang="lang"
+      :key="key">
+      hej
+    </OrderItem>
   </div>
+</div>
+</div>
 </div>
 </template>
 <script>
@@ -61,6 +61,8 @@ import OrderItem from '@/components/OrderItem.vue'
 //import FrontPage from '@/components/FrontPage.vue'
 //import methods and data that are shared between ordering and kitchen views
 import sharedVueStuff from '@/components/sharedVueStuff.js'
+import MenuPage from '@/components/MenuPage.vue'
+
 
 /* instead of defining a Vue instance, export default allows the only
 necessary Vue instance (found in main.js) to import your data and methods */
@@ -69,15 +71,19 @@ export default {
   components: {
     Ingredient,
     OrderItem,
+    MenuPage,
     //FrontPage
   },
   mixins: [sharedVueStuff], // include stuff that is used in both
-                            // the ordering system and the kitchen
+  // the ordering system and the kitchen
+
   data: function() { //Not that data is a function!
     return {
       chosenIngredients: [],
       price: 0,
       orderNumber: "",
+      showMainMenu: true,
+      showOrdering: false
     }
   },
   created: function () {
@@ -86,17 +92,23 @@ export default {
     }.bind(this));
   },
   methods: {
+    klappatochKlart: function () {
+      this.showOrdering = true;
+      this.showMainMenu = false;
+    },
+
     addToOrder: function (item) {
       this.chosenIngredients.push(item);
       this.price += +item.selling_price;
     },
+
     placeOrder: function () {
       var i,
       //Wrap the order in an object
-        order = {
-          ingredients: this.chosenIngredients,
-          price: this.price
-        };
+      order = {
+        ingredients: this.chosenIngredients,
+        price: this.price
+      };
       // make use of socket.io's magic to send the stuff to the kitchen via the server (app.js)
       this.$store.state.socket.emit('order', {order: order});
       //set all counters to 0. Notice the use of $refs
