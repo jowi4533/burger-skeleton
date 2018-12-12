1fab4777109333421cc30d0a038579e1e2c8f74f
SebastianTapper <s.tapper.97@gmail.com>
2018-12-12 15:15:19 +0100

    HomePage Done

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,17 +1,22 @@
 <template>
 
-<div>
+<body id="body">
+
+
+<div id="all">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
 <header id="TopHeader">
   <img id="headerImg" src="https://i.imgur.com/1NLTXH0.png">
 </header>
 
   <div class="wrapper" align="center">
-      <button id="EatHere" v-on:click="OrderOption()">Eat Here</button>
+      <button id="EatHere" v-on:click="OrderOption()" >Eat Here</button>
       <button id="EatThere" v-on:click="OrderOption()">Take Away</button>
 
   </div>
 </div>
+</body>
 </template>
 
 
@@ -36,10 +41,13 @@ export default {
 
 
 <style lang="css">
+#body{
+  background-color: black;
+  overflow: hidden;
+}
 #TopHeader{
   font-size: 2em;
   text-align: center;
-  overflow: hidden;
   background-color: black;
 
 
@@ -54,10 +62,11 @@ export default {
 
 .wrapper{
   display: grid;
-  grid-gap: 5%;
-  grid-template-columns: 20% 20% 20% 20% 20%;
-  align-items: center;
+  grid-gap: 3%;
+  grid-template-columns: 50% 50%;
+  color: black;
   background-color: black;
+
 
 
 
@@ -66,16 +75,18 @@ export default {
 }
 #EatHere{
 /*  position: absolute;*/
-  width: 6em;
-  height: 3em;
+  display: block;
   color: green;
-  font-size: 3em;
-  border: 2px dashed black;
-  margin-top: 30%;
-  margin-bottom:50%;
+  font-size: 200%;
+  width: 80%;
   background-color: lightgrey;
-  grid-column: 2;
+  margin: 5%;
+  margin-bottom: 30%;
+  margin-top: 30%;
+  border: dashed 2px black;
+  grid-column: 1;
   grid-row: 1;
+
 
 
 
@@ -85,17 +96,19 @@ export default {
 
 }
 #EatThere{
-  width:6em;
-  height: 3em;
+  display: block;
   color: green;
-  font-size: 3em;
+  font-size: 200%;
+  width: 80%;
   text: bold;
-  border: 2px dashed black;
-  background-color: lightgrey;
+  margin: 5%;
+  margin-bottom: 30%;
   margin-top: 30%;
-  margin-bottom: 50%;
-  grid-column: 3;
-  grid-row:1;
+  padding: 5%;
+  background-color: lightgrey;
+  border: dashed 2px black;
+  grid-column: 2;
+  grid-row: 1;
 }
 #EatThere:hover{
   background-color: darkblue;
