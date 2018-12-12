80173a37157cd823a5c3e765303cf0f104ba72bb
SebastianTapper <s.tapper.97@gmail.com>
2018-12-12 18:18:58 +0100

    hej

--- src/router.js
+++ src/router.js
@@ -1,15 +1,14 @@
+//import Kitchen from './views/Kitchen.vue'
 import Vue from 'vue'
 import Router from 'vue-router'
-import OverView from './components/OverView.vue'
-import Kitchen from './views/Kitchen.vue'
+import Ordering from './views/Ordering.vue'
 
 Vue.use(Router)
 
 let router = new Router({
   routes: [
-
     {
-      path: '/OverView',
+      path: '/',
       meta: {
         title: 'Raw Sauce',
       },
@@ -21,17 +20,28 @@ let router = new Router({
       meta: {
         title: 'Kraft Burgers',
       },
-      name: 'overView',
-      component: OverView
+      name: 'ordering',
+      component: Ordering
     },
-
+    // {
+    //   path: '/frontpage',
+    //   meta: {
+    //     title: 'RAW Sauce Burgers',
+    //   },
+    //   name: 'frontpage',
+    //   component: () => import( './views/FrontPage.vue')
+    //
+    // },
     {
       path: '/kitchen',
       meta: {
         title: 'Kitchen',
       },
       name: 'kitchen',
-      component: Kitchen
+      // route level code-splitting
+      // this generates a separate chunk (read.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "read" */ './views/Kitchen.vue')
     },
 
   ]
