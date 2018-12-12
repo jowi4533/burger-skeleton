8381094fb823736f05f2f0f888e22035126cf8ef
Jonas Wikström <jonaswikstrom@student-225-87.eduroam.uu.se>
2018-12-12 18:29:33 +0100

    min grupp är cp

---
+++ src/components/StageHeader.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="">
+
+  </div>
+</template>
+<script>
+export default {
+  name: 'Ingredient',
+  props: {
+    item: Object,
+    lang: String,
+    uiLabels: Object
+  },
+    data: function () {
+    return {
+      counter: 0
+    };
+  },
+  methods: {
+    incrementCounter: function () {
+      this.counter += 1;
+      // sending 'increment' message to parent component or view so that it
+      // can catch it with v-on:increment in the component declaration
+      this.$emit('increment');
+    },
+    resetCounter: function () {
+      this.counter = 0;
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
