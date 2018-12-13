de5485f78657825a56e0ed88e665b80e476ad11e
Jonteton <jonteton@gmail.com>
2018-12-13 14:55:55 +0100

    swag

--- src/components/BuildYourBurger/Vegetables.vue
+++ src/components/BuildYourBurger/Vegetables.vue
@@ -1,16 +1,44 @@
 <template>
-<div>
+<div id = "ToppingsAndSauceContainer">
 <h1> Hello! This is the Vegetables, whalecum! </h1>
 
+  <button v-on:click= "switchToBreadAndPatty()">Bread and Patty</button>
+  <button v-on:click= "switchToToppingsAndSauce()">Toppings and Sauce</button>
+  <button>Vegetables</button>
+  <button v-on:click= "switchToToppingsAndSauce()">Previous</button>
+  <button v-on:click= "switchToDrinks()">Go to Vegetables (next)</button>
+
 </div>
+
 </template>
 
-
-
 <script>
+
 export default{
-  name: 'MenuPage',
+  name: 'BreadAndPatty',
+
+  data: function() {
+    return {
+    }
+  },
+
+  methods: {
+
+    switchToVegetables: function() {
+      this.$parent.state = "Vegetables"
+    },
+
+    switchToBreadAndPatty: function() {
+      this.$parent.state = "BreadAndPatty";
+    },
+
+    switchToDrinks: function() {
+      this.$parent.state = "Drinks";
+    }
+  }
 }
+
+
 </script>
 
 
