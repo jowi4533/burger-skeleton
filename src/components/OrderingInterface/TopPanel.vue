fade3ae1ed54a658dc403a3d1f5df33eb4d3387a
Jonteton <jonteton@gmail.com>
2018-12-18 12:55:57 +0100

    Hej

--- src/components/OrderingInterface/TopPanel.vue
+++ src/components/OrderingInterface/TopPanel.vue
@@ -2,32 +2,32 @@
   <div id = "TopPanelContainer">
 
     <div id = "buildYourBurgerPage" v-if = "parentState === 'BreadAndPatty' ||
-      parentState === 'ToppingsAndSauce' || parentState === 'Vegetables' ">
-      <button> 1 </button>
-      <button v-on:click= "switchToDrinks()">2</button>
-      <button v-on:click= "switchToOverView()">3</button>
-      <h1> Hello this is 1: Build Your Burger </h1>
-      <button v-on:click= "switchToMenuPage()">Cancel</button>
-    </div>
-
-
-    <div id = "sidesAndDrinksPage" v-if = "parentState === 'Drinks' || parentState === 'Sides'">
-      <button v-on:click= "switchToBreadAndPatty()">1</button>
-      <button> 2 </button>
-      <button v-on:click= "switchToOverView()">3</button>
-      <h1> Hello this is 2: Sides and Drinks </h1>
-      <button v-on:click= "switchToMenuPage()">Cancel</button>
-    </div>
-
-    <div id = "overViewPage" v-if = "parentState === 'OverView'">
-      <button v-on:click= "switchToBreadAndPatty()">1</button>
-      <button v-on:click= "switchToDrinks()">2</button>
-      <button> 3 </button>
-      <h1> Hello this is 3: Overview </h1>
-      <button v-on:click= "switchToMenuPage()">Cancel</button>
-    </div>
-
+    parentState === 'ToppingsAndSauce' || parentState === 'Vegetables' ">
+    <button> 1 </button>
+    <button v-on:click= "switchToDrinks()">2</button>
+    <button v-on:click= "switchToOverView()">3</button>
+    <h1> Hello this is 1: Build Your Burger </h1>
+    <button v-on:click= "switchToMenuPage()">Cancel</button>
   </div>
+
+
+  <div id = "sidesAndDrinksPage" v-if = "parentState === 'Drinks' || parentState === 'Sides'">
+    <button v-on:click= "switchToBreadAndPatty()">1</button>
+    <button> 2 </button>
+    <button v-on:click= "switchToOverView()">3</button>
+    <h1> Hello this is 2: Sides and Drinks </h1>
+    <button v-on:click= "switchToMenuPage()">Cancel</button>
+  </div>
+
+  <div id = "overViewPage" v-if = "parentState === 'OverView'">
+    <button v-on:click= "switchToBreadAndPatty()">1</button>
+    <button v-on:click= "switchToDrinks()">2</button>
+    <button> 3 </button>
+    <h1> Hello this is 3: Overview </h1>
+    <button v-on:click= "switchToMenuPage()">Cancel</button>
+  </div>
+
+</div>
 </template>
 
 <script>
@@ -58,13 +58,7 @@ export default{
     switchToOverView: function() {
       this.$parent.state = "OverView"
     },
-  },
-
-  computed: {
-    updateParentState: function() {
-      parentState=this.$parent.state;
   }
-}
 }
 
 
