f35e97f408ea993707d868a3ee13bbde07c3f90d
Jonteton <jonteton@gmail.com>
2018-12-19 14:53:32 +0100

    hej

---
+++ src/components/Payment.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id = "PaymentContainer">
+    <h1> Hello! This is the Payment whalecum! </h1>
+
+    <button v-on:click= "switchToSides()">Previous</button>
+    <button> Add item to Order</button>
+    <button> Edit </button>
+    <button>Purchase selected items</button>
+
+  </div>
+
+</template>
+
+<script>
+
+export default{
+  name: 'overview',
+
+  data: function() {
+    return {
+    }
+  },
+
+  methods: {
+
+    switchToSides: function() {
+      this.$parent.state = "Sides";
+    }
+  }
+}
+
+
+</script>
+
+
+<style scoped>
+
+
+</style>
