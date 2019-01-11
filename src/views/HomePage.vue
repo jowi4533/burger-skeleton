b1854bd5bf75949adcf8c74216a0db85348209a4
Jakob Nikamo <Jakob@student-231-83.eduroam.uu.se>
2019-01-11 12:02:22 +0100

    lagatin en till bidl

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,19 +3,18 @@
 <body id="body">
 
 
-<div id="all">
+
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
 <header id="TopHeader">
   <img id="headerImg" src="https://i.imgur.com/1NLTXH0.png">
-
 </header>
 
   <div class="wrapper" align="center">
       <button id="EatHere" v-on:click="OrderOption()" >Eat Here</button>
       <button id="EatThere" v-on:click="OrderOption()">Take Away</button>
   </div>
-</div>
+
 </body>
 </template>
 
@@ -35,77 +34,65 @@ export default {
 </script>
 
 <style lang="css">
+
 #body{
-  background-color: black;
-  overflow: hidden;
+  background-color: rgb(192, 239, 232);
+/*  overflow: hidden;*/
 }
+
 #TopHeader{
   font-size: 2em;
   text-align: center;
   background-color: black;
-
-
 }
+
 #headerImg{
   width: 100%;
   opacity: 0.8;
-  border-bottom: 2px dashed red;
+  border-bottom: 2px solid red;
 }
-
-
 
 .wrapper{
   display: grid;
-  grid-gap: 3%;
   grid-template-columns: 50% 50%;
   color: black;
-  background-color: black;
-
-
-
-
-
-
+  height: 60vh;
+  /*background-color: rgb(192, 239, 232);*/
 }
+
+button {
+  background-color: rgb(0,200,250);
+  color: rgb(4, 40, 51);
+  font-size: 200%;
+  width: 80%;
+  margin: 5% 0 5% 0;
+  opacity: 0.7;
+  transition: 0.3s;
+  border-style: hidden;
+  border-radius: 10px;
+}
+
 #EatHere{
 /*  position: absolute;*/
-  display: block;
-  color: green;
-  font-size: 200%;
-  width: 80%;
-  background-color: lightgrey;
-  margin: 5%;
-  margin-bottom: 30%;
-  margin-top: 30%;
+  /*display: block;*/
   grid-column: 1;
   grid-row: 1;
-  opacity: 0.7;
-
-
-
-
 }
+
 #EatHere:hover{
   opacity: 1;
-
+  cursor: pointer;
 }
+
 #EatThere{
-  display: block;
-  color: green;
-  font-size: 200%;
-  width: 80%;
-  text: bold;
-  margin: 5%;
-  margin-bottom: 30%;
-  margin-top: 30%;
-  padding: 5%;
-  background-color: lightgrey;
+  /*display: block;*/
   grid-column: 2;
   grid-row: 1;
-  opacity: 0.7;
 }
+
 #EatThere:hover{
   opacity: 1;
+  cursor: pointer;
 }
 
 </style>
